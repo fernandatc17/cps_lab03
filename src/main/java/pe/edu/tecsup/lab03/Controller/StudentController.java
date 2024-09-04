a1aa7a49c46ff9187c5339783ae16ae4153febde
fernandatc17 <abigailticlavilca46@gmail.com>
2024-09-04 17:29:52 -0500

    Fernanda2

--- src/main/java/pe/edu/tecsup/lab03/Controller/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/Controller/StudentController.java
@@ -2,8 +2,31 @@ package pe.edu.tecsup.lab03.Controller;
 
 public class StudentController {
 
-    public String getStudents() {
-        return "Lista de estudiantes";
+    private String schoolName;
+    private int totalStudents;
+
+    // Constructor
+    public StudentController() {
+        this.schoolName = "TECSUP";
+        this.totalStudents = 1000;
     }
 
+    public String getStudents() {
+        return "Lista de estudiantes de " + schoolName + " con un total de " + totalStudents + " estudiantes.";
+    }
+
+    public String getStudentById(int id) {
+        return "Información del estudiante con ID: " + id;
+    }
+
+    public String updateStudent(int id, String studentInfo) {
+        return "Actualizando información del estudiante con ID: " + id + " a: " + studentInfo;
+    }
+
+    public static void main(String[] args) {
+        StudentController controller = new StudentController();
+        System.out.println(controller.getStudents());
+        System.out.println(controller.getStudentById(1));
+        System.out.println(controller.updateStudent(1, "Nuevo estudiante"));
+    }
 }
